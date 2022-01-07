5da348f4e9e7d550ed60d93e68ddc581b0b92b60
Thanika <90124926+Thanika1979@users.noreply.github.com>
2022-01-07 09:48:47 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+function myFunction(){
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for(i = 0; i < dropdowns.length; i++){
+            var openDropdown = dropdowns[i];
+            if(openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+//Sign up
+
+var dropdown = document.getElementsByClassName("signbtn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
+
+
